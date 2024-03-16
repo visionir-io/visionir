cccb074d39e7dd191ad0c5c0f95a0e80be8adafb
Nir Abutbul <visionir@icloud.com>
2024-03-16 20:43:41 +0200

    adding paychecks calculator

--- apps/paychecks/app/src/main.py
+++ apps/paychecks/app/src/main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title('Paychecks App is  coming')
+with st.form(key="paycheck calculation"):
+    st.write("Welcome to the paycheck calculator")
+    st.write("Enter your details below")
+    total_money = st.number_input("Enter your total money", min_value=0.0)
+    interest_paid = st.number_input("Enter the interest paid", min_value=0.0)
+    if st.form_submit_button("Calculate"):
+        st.write(f"Your interest rate is: {interest_paid / total_money * 100:.2f}%")

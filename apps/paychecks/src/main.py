b7ab71fdc5db4fc3c0fd161181b3c84745529cf3
Nir Abutbul <visionir@icloud.com>
2024-04-04 00:38:40 +0300

    consolodating composes and devcontainers

--- apps/paychecks/src/main.py
+++ apps/paychecks/src/main.py
@@ -1,13 +1,33 @@
+from time import sleep
+
+import pyroscope_io
 import streamlit as st
 
-with st.form(key='paycheck calculation'):
-    st.write('Welcome to the paycheck calculator')
-    st.write('Enter your details below')
-    check_amount = st.number_input('Enter the check amount', min_value=0)
-    interest_paid = st.number_input('Enter the interest paid', min_value=0)
-    days_to_pay = st.number_input('Enter the days to collect the money', min_value=0)
-    if st.form_submit_button('Calculate'):
-        daily_interest_rate = interest_paid / days_to_pay
-        monthly_value = daily_interest_rate * 30
-        check_intetest = (monthly_value / check_amount) * 100
-        st.write(f"monthly value interest rate: {check_intetest:.2f}%")
+# pyroscope_io.configure(
+#     application_name="my.python.app",  # replace this with some name for your application
+#     server_address="http://localhost:4040",  # replace this with the address of your Pyroscope server
+#     sample_rate=100,  # default is 100
+#     detect_subprocesses=False,  # detect subprocesses started by the main process; default is False
+#     oncpu=True,  # report cpu time only; default is True
+#     gil_only=True,  # only include traces for threads that are holding on to the Global Interpreter Lock; default is True
+#     enable_logging=True,  # does enable logging facility; default is False
+#     tags={
+#         "app": "paychecks",
+#     },
+# )
+
+with pyroscope_io.tag_wrapper({"app": "paychecks"}):
+    with st.form(key="paycheck calculation"):
+        st.write("Welcome to the paycheck calculator")
+        st.write("Enter your details below")
+        check_amount = st.number_input("Enter the check amount", min_value=0)
+        interest_paid = st.number_input("Enter the interest paid", min_value=0)
+        days_to_pay = st.number_input(
+            "Enter the days to collect the money", min_value=0
+        )
+        if st.form_submit_button("Calculate"):
+            sleep(5)
+            daily_interest_rate = interest_paid / days_to_pay
+            monthly_value = daily_interest_rate * 30
+            check_intetest = (monthly_value / check_amount) * 100
+            st.write(f"monthly value interest rate: {check_intetest:.2f}%")

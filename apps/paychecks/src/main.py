299344de52187d81e1626920b84d7bd4fb023b80
Nir Abutbul <visionir@icloud.com>
2024-04-05 22:51:58 +0300

    quickfix for paychecks image

--- apps/paychecks/src/main.py
+++ apps/paychecks/src/main.py
@@ -1,13 +1,18 @@
 import streamlit as st
+from utils import logger
 
-with st.form(key="paycheck calculation"):
-    st.write("Welcome to the paycheck calculator")
-    st.write("Enter your details below")
-    check_amount = st.number_input("Enter the check amount", min_value=0)
-    interest_paid = st.number_input("Enter the interest paid", min_value=0)
-    days_to_pay = st.number_input("Enter the days to collect the money", min_value=0)
-    if st.form_submit_button("Calculate"):
-        daily_interest_rate = interest_paid / days_to_pay
-        monthly_value = daily_interest_rate * 30
-        check_intetest = (monthly_value / check_amount) * 100
-        st.write(f"monthly value interest rate: {check_intetest:.2f}%")
+if __name__ == "__main__":
+    with st.form(key="paycheck calculation"):
+        st.write("Welcome to the paycheck calculator")
+        st.write("Enter your details below")
+        check_amount = st.number_input("Enter the check amount", min_value=0)
+        interest_paid = st.number_input("Enter the interest paid", min_value=0)
+        days_to_pay = st.number_input(
+            "Enter the days to collect the money", min_value=0
+        )
+        if st.form_submit_button("Calculate"):
+            daily_interest_rate = interest_paid / days_to_pay
+            monthly_value = daily_interest_rate * 30
+            check_intetest = (monthly_value / check_amount) * 100
+            st.write(f"monthly value interest rate: {check_intetest:.2f}%")
+            logger.debug(f"monthly value interest rate: {check_intetest:.2f}%")

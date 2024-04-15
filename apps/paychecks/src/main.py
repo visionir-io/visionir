bedf5a6fae1eff80d33aed1b39e224d2e542b056
Nir Abutbul <visionir@icloud.com>
2024-04-16 02:46:25 +0300

    adding pyroscope tags and logging facility

--- apps/paychecks/src/main.py
+++ apps/paychecks/src/main.py
@@ -1,15 +1,28 @@
 import streamlit as st
-from logger import Logger
 
 
-@st.cache_resource()
+@st.cache_resource
 def get_logger():
-    return Logger().init_logger("paychecks")
+    from observability import observer as obs
+
+    return obs
 
 
+obs = get_logger()
+
+
+@st.cache_data
+@obs.tag_wrapping({"function": "calculate_interest"})
+def calculate_interest(check_amount: float, interest_paid: float, days_to_pay: float):
+    daily_interest_rate = interest_paid / days_to_pay
+    monthly_value = daily_interest_rate * 30
+    check_intetest = (monthly_value / check_amount) * 100
+    return check_intetest
+
+
+@obs.tag_wrapping({"function": "main"})
 def main():
-    logger = get_logger()
-    logger.debug("Application started")
+    obs.logger.debug("Application started")
     with st.form(key="paycheck calculation"):
         st.write("Welcome to the paycheck calculator")
         st.write("Enter your details below")
@@ -19,14 +32,14 @@ def main():
             "Enter the days to collect the money", min_value=0
         )
         if st.form_submit_button("Calculate"):
-            logger.info(
-                f"params received: {check_amount=}, {interest_paid=}, {days_to_pay=}"
+            obs.logger.info(
+                f"params received: {check_amount=},{interest_paid=},{days_to_pay=}"
             )
-            daily_interest_rate = interest_paid / days_to_pay
-            monthly_value = daily_interest_rate * 30
-            check_intetest = (monthly_value / check_amount) * 100
+            check_intetest = calculate_interest(
+                check_amount, interest_paid, days_to_pay
+            )
             st.write(f"monthly value interest rate: {check_intetest:.2f}%")
-            logger.debug(f"monthly value interest rate: {check_intetest:.2f}%")
+            obs.logger.debug(f"monthly value interest rate: {check_intetest:.2f}%")
 
 
 if __name__ == "__main__":

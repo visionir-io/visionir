21194150ea14ec00d368886896fa6f44808999bf
Nir Abutbul <nir@visionir.io>
2024-06-06 23:10:47 +0300

    align repo

---
+++ infra/terraform/README.md
@@ -0,0 +1,2 @@
+In cloudflare, there is no special record for each topic provider (sendgrid, icloud)
+for sendgrind, the records were created via terraform. to avoid complexity, icloud records were created automatically. hence, terraform isn't aware of iCloud.
--- infra/terraform/cloudflare.tf
+++ infra/terraform/cloudflare.tf
@@ -24,34 +24,40 @@ locals {
       name : "_dmarc.visionir.io",
     value : "v=DMARC1; p=none; rua=mailto:postmaster@visionir.io" },
   ]
-  zoho_records = [
+  icloud_records = [
     { type : "TXT",
       name : "@",
-    value : "zoho-verification=zb80761353.zmverify.zoho.com" },
+      value : "apple-domain=Aiwr3bj9ZmjKRW0k",
+    ttl : 3600 },
     { type : "MX",
       name : "@",
-      value : "mx.zoho.com",
+      value : "mx01.mail.icloud.com.",
+      ttl : 3600
     priority : 10 },
     { type : "MX",
       name : "@",
-      value : "mx2.zoho.com",
-    priority : 20 },
-    { type : "MX",
-      name : "@",
-      value : "mx3.zoho.com",
-      priority : 50,
+      value : "mx02.mail.icloud.com.",
+      ttl : 3600
+    priority : 10 },
+    { type : "CNAME",
+      name : "sig1._domainkey",
+      value : "sig1.dkim.visionir.io.at.icloudmailadmin.com.",
+      ttl : 3600
     },
     { type : "TXT",
       name : "@",
-    value : "v=spf1 include:zoho.com ~all" },
-    {
-      type : "TXT",
-      name : "zmail._domainkey",
-      value : var.zoho_txt_record_value
-    }
+      value : "=spf1 include:zoho.com include:icloud.com ~all",
+    ttl : 3600 }
+  ]
+  discord_records = [
+    { type : "TXT",
+      name : "_discord.visionir.io",
+    value : "dh=4d3a9ab6eabe4fc2ef6c0727356d9ec0fd3efa23" },
+  ]
+  observability_apps = [
+    { "name" : "pyroscope" }, { "name" : "grafana" }, { "name" : "alloy" }
   ]
 }
-
 
 resource "cloudflare_r2_bucket" "visionir-bucket" {
   account_id = var.cloudflare_account_id
@@ -102,28 +108,50 @@ resource "cloudflare_record" "sendgrid_records" {
   proxied         = false
   depends_on      = [oci_core_instance.visionir, cloudflare_zone.visionir_io]
 }
-
-resource "cloudflare_record" "pyroscope" {
+resource "cloudflare_record" "discord_records" {
+  for_each        = { for idx, record in local.discord_records : idx => record }
   zone_id         = cloudflare_zone.visionir_io.id
   allow_overwrite = true
-  name            = "pyroscope"
-  value           = cloudflare_record.root_domain.name
-  type            = "CNAME"
+  name            = each.value.name
+  value           = each.value.value
+  type            = each.value.type
   ttl             = 1
-  proxied         = true
-  depends_on      = [oci_core_instance.visionir, cloudflare_zone.visionir_io, cloudflare_record.root_domain]
+  proxied         = false
+  depends_on      = [oci_core_instance.visionir, cloudflare_zone.visionir_io]
 }
-resource "cloudflare_record" "zoho_records" {
-  for_each        = { for idx, record in local.zoho_records : idx => record }
+
+resource "cloudflare_record" "icloud_records" {
+  for_each        = { for idx, record in local.icloud_records : idx => record }
   zone_id         = cloudflare_zone.visionir_io.id
   allow_overwrite = true
   name            = each.value.name
   value           = each.value.value
   type            = each.value.type
   priority        = lookup(each.value, "priority", null)
-  ttl             = 1
+  ttl             = each.value.ttl
   proxied         = false
   depends_on      = [oci_core_instance.visionir, cloudflare_zone.visionir_io]
+}
+resource "cloudflare_record" "observability_apps" {
+  for_each        = { for idx, record in local.observability_apps : idx => record }
+  zone_id         = cloudflare_zone.visionir_io.id
+  allow_overwrite = true
+  name            = each.value.name
+  value           = cloudflare_record.root_domain.name
+  type            = "CNAME"
+  ttl             = 1
+  proxied         = true
+  depends_on      = [oci_core_instance.visionir, cloudflare_zone.visionir_io, cloudflare_record.root_domain]
+}
+resource "cloudflare_record" "minio" {
+  zone_id         = cloudflare_zone.visionir_io.id
+  allow_overwrite = true
+  name            = "minio"
+  value           = cloudflare_record.root_domain.name
+  type            = "CNAME"
+  ttl             = 1
+  proxied         = true
+  depends_on      = [oci_core_instance.visionir, cloudflare_zone.visionir_io, cloudflare_record.root_domain]
 }
 
 output "visionir_io_nameservers" {
@@ -134,8 +162,4 @@ output "visionir_io_nameservers" {
 output "zone_id" {
   value     = cloudflare_zone.visionir_io.id
   sensitive = true
-}
-
-output "sendgrid_records" {
-  value = { for idx, record in local.sendgrid_records : idx => record.name }
 }
